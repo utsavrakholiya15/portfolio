e1d1b13551d4e35b84769ecce87013424abefc80
utsavrakholiya15 <rakholiyautsav@gmail.com>
2025-01-13 11:52:53 +0530

    changes pushed

--- src/routs/home/projects/index.js
+++ src/routs/home/projects/index.js
@@ -1,14 +1,15 @@
-import React from 'react'
-import './projects.scss';
-import qaImg from '../../../assets/IMAGEs/qa.png';
-import quizImg from '../../../assets/IMAGEs/quiz.png';
-import ageCalImg from '../../../assets/IMAGEs/age-cal.png';
-import toDoImg from '../../../assets/IMAGEs/todo.png';
+import React from "react";
+import "./projects.scss";
+import qaImg from "../../../assets/IMAGEs/qa.png";
+import quizImg from "../../../assets/IMAGEs/quiz.png";
+import ageCalImg from "../../../assets/IMAGEs/age-cal.png";
+import toDoImg from "../../../assets/IMAGEs/todo.png";
+import shopImg from "../../../assets/IMAGEs/shop.png";
 export default function Projects() {
   const scrollToBottom = () => {
     window.scrollTo({
       top: document.body.scrollHeight, // Scroll to the bottom of the page
-      behavior: 'smooth', // Smooth scrolling
+      behavior: "smooth", // Smooth scrolling
     });
   };
   const handleOnUrl = (event) => {
@@ -26,7 +27,7 @@ export default function Projects() {
         <div className="projects-item-data">
           <h4>{title}</h4>
           <ul>
-            <li>{skills}</li>
+            {/* <li>{skills}</li> */}
             {desc ? <li>{desc}</li> : null}
           </ul>
         </div>
@@ -39,36 +40,43 @@ export default function Projects() {
         <div className="projects">
           <div className="projects-title">
             <h2>Projects</h2>
-            <button onClick={scrollToBottom} >Contact me</button>
+            <button onClick={scrollToBottom}>Contact me</button>
           </div>
           <div className="projects-grid">
             <ProjectsItem
               img={quizImg}
               skills={"HTML SCSS React"}
-              desc={"okay"}
+              desc={"A fun and interactive way to test your knowledge, built using HTML, CSS, JavaScript, and React to create a smooth and engaging quiz experience."}
               url={"https://utsav-rakholiya-projects.vercel.app/quiz"}
               title={"Quiz App"}
             />
             <ProjectsItem
               img={qaImg}
               skills={"HTML SCSS React"}
-              desc={"okay"}
+              desc={"A simple yet effective way to explore JavaScript concepts, featuring interactive question-and-answer dropdowns for easy learning."}
               url={"https://utsav-rakholiya-projects.vercel.app/"}
               title={"Dropdowns"}
             />
             <ProjectsItem
               img={ageCalImg}
               skills={"Tailwind, React"}
-              desc={"okay"}
+              desc={" An accurate and easy-to-use age calculator that helps you determine your exact age down to the days, months, and years."}
               url={"https://utsav-rakholiya-projects.vercel.app/age-cal"}
               title={"Age Calculator"}
             />
             <ProjectsItem
               img={toDoImg}
               skills={"Tailwind, React"}
-              desc={"okay"}
+              desc={"A simple yet effective to-do list app that helps you organize tasks, set priorities, and stay productive with a clean and intuitive interface."}
               url={"https://utsav-rakholiya-projects.vercel.app/todo"}
               title={"To Do List"}
+            />
+            <ProjectsItem
+              img={shopImg}
+              skills={"Redux, React"}
+              desc={"A fully functional online shop with a smooth cart system, letting users add, remove, and review products effortlessly before making a purchase."}
+              url={"https://discovery--three.vercel.app/shop"}
+              title={"Shop and Cart"}
             />
           </div>
         </div>
--- src/routs/home/websites/index.js
+++ src/routs/home/websites/index.js
@@ -4,6 +4,9 @@ import biographImg from "../../../assets/IMAGEs/biograph-img.png";
 import siroImg from "../../../assets/IMAGEs/siro-img.png";
 import bofImg from "../../../assets/IMAGEs/bof-img.png";
 import cleanicImg from "../../../assets/IMAGEs/cleanic-img.png";
+import discoveryImg from "../../../assets/IMAGEs/discovery.png";
+import findmyaitoolImg from "../../../assets/IMAGEs/findmyaitool.png";
+import healthIQImg from "../../../assets/IMAGEs/healthIQ.png";
 export default function Websites() {
   const scrollToBottom = () => {
     window.scrollTo({
@@ -26,7 +29,7 @@ export default function Websites() {
         <div className="websites-item-data">
           <h4>{title}</h4>
           <ul>
-            <li>{skills}</li>
+            {/* <li>{skills}</li> */}
             {desc ? <li>{desc}</li> : null}
           </ul>
         </div>
@@ -39,36 +42,57 @@ export default function Websites() {
         <div className="websites">
           <div className="websites-title">
             <h2>Websites</h2>
-            <button onClick={scrollToBottom} >Contact me</button>
+            <button onClick={scrollToBottom}>Contact me</button>
           </div>
           <div className="websites-grid">
             <WebsiteItem
               img={biographImg}
-              skills={"HTML SCSS React"}
-              desc={"okay"}
+              skills={"React, Sass"}
+              desc={" Revolutionizing healthcare with a tech-driven hospital and medical platform."}
               url={"https://biograph-ten.vercel.app/"}
               title={"Biograph"}
             />
             <WebsiteItem
               img={siroImg}
-              skills={"HTML SCSS React"}
-              desc={"okay"}
+              skills={"React Sass"}
+              desc={"Experience the pinnacle of luxury at Dubai's premier hotel destination."}
               url={"https://siro-hotels.vercel.app/"}
               title={"Siro"}
             />
             <WebsiteItem
+              img={findmyaitoolImg}
+              skills={"React Sass"}
+              desc={"Explore top AI tools with a sleek and stunning design. 🚀"}
+              url={"https://findmyaitool.vercel.app/"}
+              title={"Find my AI tool"}
+            />
+            <WebsiteItem
               img={bofImg}
               skills={"Tailwind, React"}
-              desc={"okay"}
+              desc={"A news homepage designed with Tailwind CSS, featuring a clean, responsive layout for seamless browsing of the latest headlines."}
               url={"https://bof-three.vercel.app/"}
               title={"The Bussiness of Fashion"}
             />
             <WebsiteItem
               img={cleanicImg}
               skills={"Tailwind, React"}
-              desc={"okay"}
+              desc={"A visually stunning homepage built with Tailwind CSS, offering a seamless experience for booking home and room cleaning services."}
               url={"https://cleanic.vercel.app/"}
               title={"The Cleanic"}
+            />
+            <WebsiteItem
+              img={healthIQImg}
+              skills={"Tailwind, React"}
+              desc={"A sleek, responsive homepage built with Tailwind CSS, providing easy access to hospital services, appointments, and healthcare information."}
+              url={"https://health-iq-nu.vercel.app/"}
+              title={"Health IQ"}
+            />
+            <WebsiteItem
+              img={discoveryImg}
+              skills={"Redux, React"}
+              desc={" A school-focused homepage built with Sass and Redux, featuring a seamless shopping and cart functionality for an enhanced user experience."}
+              url={"https://discovery--three.vercel.app/"}
+              title={"Discovery"}
             />
           </div>
         </div>

1efd9c4d923c82b31f066a2eb8c246aa22321e5f
utsavrakholiya15 <rakholiyautsav@gmail.com>
2025-01-27 12:03:08 +0530

    resume added

--- src/shared/components/header/index.js
+++ src/shared/components/header/index.js
@@ -5,12 +5,21 @@ import { FaLinkedin } from "react-icons/fa6";
 import { RiVercelFill } from "react-icons/ri";
 import { SlEnvolopeLetter } from "react-icons/sl";
 import { useNavigate } from "react-router-dom";
+import resumePDF from "../../../assets/PDFs/utsav-rakholiya-resume.pdf";
 export default function Header() {
   const handleOnUrl = (url) => {
     if (url) {
       window.open(url, "_blank");
     }
   };
+
+  const handleDownload = () => {
+    const link = document.createElement("a");
+    link.href = resumePDF;
+    link.download = "utsav-rakholiya-resume.pdf";
+    link.click();
+  };
+
   return (
     <header>
       <div className="container h-full">
@@ -20,22 +29,33 @@ export default function Header() {
           </div>
           <div className="header-icons">
             <div className="header-icons-align">
-              <div onClick={()=>{
-                handleOnUrl("https://vercel.com/utsav-rakholiyas-projects")
-              }} className="header-icons-item">
+              <div
+                onClick={() => {
+                  handleOnUrl("https://vercel.com/utsav-rakholiyas-projects");
+                }}
+                className="header-icons-item"
+              >
                 <RiVercelFill />
               </div>
-              <div onClick={()=>{
-                handleOnUrl("https://github.com/utsavrakholiya15")
-              }} className="header-icons-item">
+              <div
+                onClick={() => {
+                  handleOnUrl("https://github.com/utsavrakholiya15");
+                }}
+                className="header-icons-item"
+              >
                 <FaGithub />
               </div>
-              <div onClick={()=>{
-                handleOnUrl("https://www.linkedin.com/in/utsav-rakholiya-52b3a8290/")
-              }} className="header-icons-item">
+              <div
+                onClick={() => {
+                  handleOnUrl(
+                    "https://www.linkedin.com/in/utsav-rakholiya-52b3a8290/"
+                  );
+                }}
+                className="header-icons-item"
+              >
                 <FaLinkedin />
               </div>
-              <div className="header-icons-item">
+              <div onClick={handleDownload} className="header-icons-item">
                 <SlEnvolopeLetter />
               </div>
             </div>
